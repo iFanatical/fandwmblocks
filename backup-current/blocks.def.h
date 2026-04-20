826b51c5d793c2574bc7792fc2fb29acab884b55
Aaron Bush <theawpportunist@gmail.com>
2026-04-20 14:43:48 -0700

    Initial commit. Built out blocks based on my scripts.

---
+++ backup-current/blocks.def.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/					/*Update Interval*/	/*Update Signal*/
+	{"",		"sleep 0.1 && $SCRIPTS/dunststatus.sh",			0,			2},
+	{"",		"$SCRIPTS/language.sh",				0,			0},
+	{"",		"$SCRIPTS/kernel.sh",				0,			0},
+	{"",		"$SCRIPTS/system-stats.sh",			5,			0},
+	{"",		"$SCRIPTS/volumeget.sh",			0,			11},
+	{"",		"$SCRIPTS/battery.sh",				30,			0},
+	{"",		"$SCRIPTS/time.sh",				1,			0},
+};
+
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
