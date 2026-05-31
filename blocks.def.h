7edf3ff2bb2060c3efbea042f2ffe9bc8d5c8a6d
Aaron Bush <theawpportunist@gmail.com>
2026-05-30 23:48:32 -0700

    Syncing up repo.

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/					/*Update Interval*/	/*Update Signal*/
-	{"",		"sleep 0.1 && $SCRIPTS/dunststatus.sh",		0,			2},
+	{"",		"$SCRIPTS/dunst.sh --status",			0,			4},
 	{"",		"$SCRIPTS/language.sh",				0,			0},
 	{"",		"$SCRIPTS/system-stats.sh",			5,			0},
-	{"",		"$SCRIPTS/volume.sh --status",		0,			11},
+	{"",		"$SCRIPTS/volume.sh --status",			0,			11},
 	{"",		"$SCRIPTS/vpn.sh --status",			0,			3},
 	{"",		"$SCRIPTS/battery.sh",				30,			0},
 	{"",		"$SCRIPTS/time.sh",				1,			0},
